3bedc6d5cc79c7251e95ff5d58f7b8f7e40b16ea
Srishty Mangutte <srishtymangutte@gmail.com>
2025-03-27 17:54:59 +0530

    fix: ensure user is logged in before requesting

--- service_requests/views.py
+++ service_requests/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from .models import ServiceRequest
 from .forms import ServiceRequestForm
 
-# View for listing requests
-class ServiceRequestListView(ListView):
+class ServiceRequestListView(LoginRequiredMixin, ListView):
     model = ServiceRequest
     template_name = 'service_requests/request_list.html'
     context_object_name = 'requests'
@@ -14,8 +12,7 @@ class ServiceRequestListView(ListView):
     def get_queryset(self):
         return ServiceRequest.objects.filter(user=self.request.user)
 
-# View for creating a new request
-class ServiceRequestCreateView(CreateView):
+class ServiceRequestCreateView(LoginRequiredMixin, CreateView):
     model = ServiceRequest
     form_class = ServiceRequestForm
     template_name = 'service_requests/request_form.html'
